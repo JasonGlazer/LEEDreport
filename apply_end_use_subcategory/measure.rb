a0d70b6709f47d3f9a02715f8261bfacd73ea0b6
JasonGlazer <jglazer@gard.com>
2017-12-01 12:21:48 -0600

    Added all instances of an object set to a sub end use category. Followed recommendations to clean up code.

--- apply_end_use_subcategory/measure.rb
+++ apply_end_use_subcategory/measure.rb
@@ -8,11 +8,11 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
 
   # human readable description
   def description
-    return "zx To aid in applying end-use subcategories to various objects so that they appear correctly in the reports that show breakdowns by end-use subcategory including EnergyPlus ABUPS End Uses by Subcateogry report and the report produced by the enhanced LEED summary report measure. "
+    return "To aid in applying end-use subcategories to various objects so that they appear correctly in the reports that show breakdowns by end-use subcategory including EnergyPlus ABUPS End Uses by Subcateogry report and the report produced by the enhanced LEED summary report measure. "
   end
   # human readable description of modeling approach
   def modeler_description
-    return "zAllow users to apply specific end-use subcategories to various components in their OpenStudio model. Specific predefined end-use subcategory tags that correspond to LEED end-uses that have special treatment in the LEED spreadsheet will be used such as Fans-Parking Garage, Interior Lighting-Process, Fans-Kitchen Ventilation, Elevators and Escalators, etc. The end-use subcategories for the following: Lights, ElectricEquipment, GasEquipment, HotWaterEquipment, SteamEquipment, OtherEquipment, Exterior:Lights, Fan:ConstantVolume, Fan:VariableVolume, Fan:OnOff, Fan:ZoneExhaust, WaterHeater:Stratified, EnergyManagementSystem:MeteredOutputVariable, Refrigeration:Condenser:AirCooled, Refrigeration:Condenser:EvaporativeCooled, Refrigeration:Condenser:WaterCooled, Refrigeration:GasCooler:AirCooled, Refrigeration:Compressor, Refrigeration:System. Refrigeration:TranscriticalSystem, Refrigeration:SecondarySystem"
+    return "Allow users to apply specific end-use subcategories to various components in their OpenStudio model. Specific predefined end-use subcategory tags that correspond to LEED end-uses that have special treatment in the LEED spreadsheet will be used such as Fans-Parking Garage, Interior Lighting-Process, Fans-Kitchen Ventilation, Elevators and Escalators, etc. The end-use subcategories for the following: Lights, ElectricEquipment, GasEquipment, HotWaterEquipment, SteamEquipment, OtherEquipment, Exterior:Lights, Fan:ConstantVolume, Fan:VariableVolume, Fan:OnOff, Fan:ZoneExhaust, WaterHeater:Stratified, EnergyManagementSystem:MeteredOutputVariable, Refrigeration:Condenser:AirCooled, Refrigeration:Condenser:EvaporativeCooled, Refrigeration:Condenser:WaterCooled, Refrigeration:GasCooler:AirCooled, Refrigeration:Compressor, Refrigeration:System. Refrigeration:TranscriticalSystem, Refrigeration:SecondarySystem"
   end
 
 
@@ -108,7 +108,10 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
         name_obj_choice << "All Objects of Type - OS:#{idd_object}"   
       end
     end    
-    name_obj_choice_obj = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('Name of Object - Type of Object', name_obj_choice, true)
+
+    name_obj_choice_obj = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('name_obj_choice_obj', name_obj_choice, true)
+    name_obj_choice_obj.setDisplayName('Name of Object - Type of Object')
+
     args << name_obj_choice_obj
     
     end_use_choice = OpenStudio::StringVector.new
@@ -158,7 +161,8 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
     end_use_choice << "Other 7"
     end_use_choice << "Other 8"
     end_use_choice << "Other 9"
-    end_use_subcategory = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('End Use Subcategory', end_use_choice, true)
+    end_use_subcategory = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('end_use_subcategory', end_use_choice, true)
+    end_use_subcategory.setDisplayName('End Use Subcategory')
     args << end_use_subcategory
     return args
   end #end the arguments method
@@ -172,17 +176,19 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
     end
     
     #assign the user inputs to variables
-    selected_name_obj = runner.getStringArgumentValue('Name of Object - Type of Object',user_arguments)
+    selected_name_obj = runner.getStringArgumentValue('name_obj_choice_obj',user_arguments)
     name_of_obj, type_of_object = selected_name_obj.split(' - OS:')
     if selected_name_obj.empty?
       runner.registerError("No object was chosen.")
+      return false
     else
       runner.registerInfo("The selected object name: '#{selected_name_obj}' with the name of the object: '#{name_of_obj}' and the type of object: '#{type_of_object}'")
     end
 
-    selected_subcategory = runner.getStringArgumentValue('End Use Subcategory',user_arguments)
+    selected_subcategory = runner.getStringArgumentValue('end_use_subcategory',user_arguments)
     if selected_subcategory.empty?
       runner.registerError("No end use subcategory was chosen.")
+      return false
     else
       runner.registerInfo("The selected end use subcategory: #{selected_subcategory}")
     end
@@ -191,7 +197,9 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
     if name_of_obj == "All Objects of Type"
       runner.registerInitialCondition("The model includes #{objs_of_selected.size} objects of type 'OS:#{type_of_object}'.")
       objs_of_selected.each do |obj|
-        obj.setEndUseSubcategory(selected_subcategory)
+        runner.registerInfo("The end-use subcategory '#{selected_subcategory}' has been applied to an object of type 'OS:#{type_of_object}' named '#{obj.name.to_s}'.")
+        specific_obj = get_specific_obj(obj, type_of_object)
+        specific_obj.setEndUseSubcategory(selected_subcategory)
       end
       runner.registerFinalCondition("The end-use subcategory '#{selected_subcategory}' has been applied to #{objs_of_selected.size} objects of type 'OS:#{type_of_object}'.")
     else # single object
@@ -213,8 +221,10 @@ class ApplyEndUseSubcategory < OpenStudio::Ruleset::ModelUserScript
       end  
       if not found
         runner.registerError("The object '#{name_of_obj}' of type 'OS:#{type_of_object}' was missing and the end-use subcategory of '#{selected_subcategory}' could not be applied.")
-      end
+       return false
+     end
     end # end if name_of_obj == "All Objects of Type"
+    return true
   end #end the run method
 
 end #end the measure
