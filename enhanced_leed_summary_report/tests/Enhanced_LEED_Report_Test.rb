7c2ce4a3929801c7790ac9afc2979741c50dc439
JasonGlazer <jglazer@gard.com>
2017-12-22 12:41:44 -0600

    Add tariff to measure. Assumes tariff_selection_flat measure is present.

--- enhanced_leed_summary_report/tests/Enhanced_LEED_Report_Test.rb
+++ enhanced_leed_summary_report/tests/Enhanced_LEED_Report_Test.rb
@@ -120,38 +120,38 @@ class EnhancedLEEDSummaryReport_Test < MiniTest::Unit::TestCase
     workspace = OpenStudio::Workspace.new("Draft".to_StrictnessLevel, "EnergyPlus".to_IddFileType)
     workspace.addObjects(idf_output_requests)
 
-    # add tariff to workspace for testing    
-    new_object_string = "
-      UtilityCost:Tariff,
-        Electricity Tariff,                     !- Name
-        ElectricityPurchased:Facility,          !- Output Meter Name
-        kWh,                                    !- Conversion Factor Choice
-        ,                                       !- Energy Conversion Factor
-        ,                                       !- Demand Conversion Factor
-        ,                                       !- Time of Use Period Schedule Name
-        ,                                       !- Season Schedule Name
-        ,                                       !- Month Schedule Name
-        Day,                                    !- Demand Window Length
-        0.0;                                    !- Monthly Charge or Variable Name
-        "
-    elec_tariff = workspace.addObject(OpenStudio::IdfObject::load(new_object_string).get).get
-
-    # make UtilityCost:Charge:Simple objects for electricity
-    new_object_string = "
-      UtilityCost:Charge:Simple,
-        ElectricityTariffEnergyCharge, !- Name
-        Electricity Tariff,                     !- Tariff Name
-        totalEnergy,                            !- Source Variable
-        Annual,                                 !- Season
-        EnergyCharges,                          !- Category Variable Name
-        0.12;                                   !- Cost per Unit Value or Variable Name
-        "
-    elec_utility_cost = workspace.addObject(OpenStudio::IdfObject::load(new_object_string).get).get
-    
-    puts "elec_tariff", elec_tariff
-    puts "elec_utility_cost", elec_utility_cost
-    puts "workspace", workspace
-    
+#    # add tariff to workspace for testing    
+#    new_object_string = "
+#      UtilityCost:Tariff,
+#        Electricity Tariff,                     !- Name
+#        ElectricityPurchased:Facility,          !- Output Meter Name
+#        kWh,                                    !- Conversion Factor Choice
+#        ,                                       !- Energy Conversion Factor
+#        ,                                       !- Demand Conversion Factor
+#        ,                                       !- Time of Use Period Schedule Name
+#        ,                                       !- Season Schedule Name
+#        ,                                       !- Month Schedule Name
+#        Day,                                    !- Demand Window Length
+#        0.0;                                    !- Monthly Charge or Variable Name
+#        "
+#    elec_tariff = workspace.addObject(OpenStudio::IdfObject::load(new_object_string).get).get
+#
+#    # make UtilityCost:Charge:Simple objects for electricity
+#    new_object_string = "
+#      UtilityCost:Charge:Simple,
+#        ElectricityTariffEnergyCharge, !- Name
+#        Electricity Tariff,                     !- Tariff Name
+#        totalEnergy,                            !- Source Variable
+#        Annual,                                 !- Season
+#        EnergyCharges,                          !- Category Variable Name
+#        0.12;                                   !- Cost per Unit Value or Variable Name
+#        "
+#    elec_utility_cost = workspace.addObject(OpenStudio::IdfObject::load(new_object_string).get).get
+#    
+#    puts "elec_tariff", elec_tariff
+#    puts "elec_utility_cost", elec_utility_cost
+#    puts "workspace", workspace
+#    
     rt = OpenStudio::EnergyPlus::ReverseTranslator.new
     request_model = rt.translateWorkspace(workspace)
 
@@ -269,4 +269,115 @@ class EnhancedLEEDSummaryReport_Test < MiniTest::Unit::TestCase
     assert(File.exist?(report_path(test_name)))
     puts "end of test_good_argument_values"
   end
+  
+ def test_tariff      
+    puts "begin test_tariff"
+    test_name = 'tariff'
+    model_in_path = "#{File.dirname(__FILE__)}/ExampleModel.osm"
+
+    # create an instance of the measure
+    measure = EnhancedLEEDSummaryReport.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+
+    # get arguments
+    arguments = measure.arguments
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # create hash of argument values
+    args_hash = {}
+
+    # populate argument with specified hash value if specified
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      if args_hash[arg.name]
+        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      end
+      argument_map[arg.name] = temp_arg_var
+    end
+
+    # get the energyplus output requests, this will be done automatically by OS App and PAT
+    idf_output_requests = measure.energyPlusOutputRequests(OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new), argument_map)
+    assert_equal(0, idf_output_requests.size)
+
+    # mimic the process of running this measure in OS App or PAT
+    epw_path = epw_path_default
+
+    # temporarily change directory to the run directory and run the measure
+    start_dir = Dir.pwd
+    begin
+      if !File.exist?(run_dir(test_name))
+        FileUtils.mkdir_p(run_dir(test_name))
+      end
+      assert(File.exist?(run_dir(test_name)))
+      Dir.chdir(run_dir(test_name))
+
+      # copy osw, osm, and epw to test directory and run from there.
+      source_osw = '../../add_tariff.osw'
+      target_osw = 'in.osw'
+      FileUtils.cp(source_osw, target_osw)
+      target_osm = 'TestOutput.osm'
+
+      # convert output requests to OSM for testing, OS App and PAT will add these to the E+ Idf
+      workspace = OpenStudio::Workspace.new("Draft".to_StrictnessLevel, "EnergyPlus".to_IddFileType)
+      workspace.addObjects(idf_output_requests)
+      rt = OpenStudio::EnergyPlus::ReverseTranslator.new
+      request_model = rt.translateWorkspace(workspace)
+
+      translator = OpenStudio::OSVersion::VersionTranslator.new
+      model = translator.loadModel(model_in_path)
+      assert((not model.empty?))
+      model = model.get
+      model.addObjects(request_model.objects)
+      model.save(target_osm, true)
+
+      # create osw that calls tariff measure. That measure needs to sit adjacent ot this one
+      cli_path = OpenStudio.getOpenStudioCLI
+      cmd = "\"#{cli_path}\" run -w \"in.osw\""
+      system(cmd)
+
+    ensure
+      Dir.chdir(start_dir)
+    end
+
+    assert(File.exist?(model_out_path(test_name)))
+    assert(File.exist?(sql_path(test_name)))
+    assert(File.exist?(epw_path))
+
+    # set up runner, this will happen automatically when measure is run in PAT or OpenStudio
+    runner.setLastOpenStudioModelPath(OpenStudio::Path.new(model_out_path(test_name)))
+    runner.setLastEnergyPlusWorkspacePath(OpenStudio::Path.new(workspace_path(test_name)))
+    runner.setLastEpwFilePath(epw_path)
+    runner.setLastEnergyPlusSqlFilePath(OpenStudio::Path.new(sql_path(test_name)))
+
+    # delete the output if it exists
+    if File.exist?(report_path(test_name))
+      FileUtils.rm(report_path(test_name))
+    end
+    assert(!File.exist?(report_path(test_name)))
+
+    # temporarily change directory to the run directory and run the measure
+    start_dir = Dir.pwd
+    begin
+      Dir.chdir(run_dir(test_name))
+
+      # run the measure
+      measure.run(runner, argument_map)
+      result = runner.result
+      show_output(result)
+      assert_equal('Success', result.value.valueName)
+
+      # look for section_errors
+      assert(section_errors(runner).size == 0)
+
+    ensure
+      Dir.chdir(start_dir)
+    end
+
+    # make sure the report file exists
+    assert(File.exist?(report_path(test_name)))
+  end
+  
+  
 end
